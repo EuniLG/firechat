d92f8c12d78ed5d6cc22577ace1ae805654f75f5
EuniLG <31998921+EuniLG@users.noreply.github.com>
2019-04-30 14:07:52 -0500

    clase 30/4
    Modificacion de chat

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,19 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC6FQxVJ5tAqz4KTpojnToamUo0IjsJ1zA',
+    authDomain: 'firechat-236a8.firebaseapp.com',
+    databaseURL: 'https://firechat-236a8.firebaseio.com',
+    projectId: 'firechat-236a8',
+    storageBucket: 'firechat-236a8.appspot.com',
+    messagingSenderId: '785275826795'
+  }
 };
-
 /*
+
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
